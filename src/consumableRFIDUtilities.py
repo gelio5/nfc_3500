41e126fcb796a908865e25eec2a452640a82da51
gelio5 <vlreznik97@gmail.com>
2024-04-08 11:37:27 +0300

    ADD some implements

--- src/consumableRFIDUtilities.py
+++ src/consumableRFIDUtilities.py
@@ -1,15 +1,182 @@
 import binascii
+import datetime
 import time
 import zlib
+
+# from builtins import int
 
 from src.settings import settings
 
 
 class ConsumableRFIDUtilities:
 
-    def __init__(self): ...
+    @staticmethod
+    def _format_number_to_4_digit_str(digit: int) -> str:
+        current_str = str(digit)
+        while len(current_str) < 4:
+            current_str = "0" + current_str
+        return current_str
 
     # ANODE BUFFER FUNCTIONS
+    @staticmethod
+    def create_anode_buffer_blob(
+        tag_uid: str,
+        buffer_type: str,
+        part_num: str,
+        lot_num: str,
+        expiration_date: int,
+        installation_date: int,
+        life_on_instrument: int,
+        runs_allowed: int,
+        runs_remaining: int,
+    ) -> str:
+        pass
+
+        blob = ""
+        blob = blob + buffer_type + ","
+        blob = blob + part_num + ","
+        blob = blob + lot_num + ","
+        blob = blob + str(expiration_date / 1000) + ","
+        blob = blob + str(installation_date / 1000) + ","
+        blob = blob + str(life_on_instrument / 1000) + ","
+
+        runs_allowed = ConsumableRFIDUtilities._format_number_to_4_digit_str(runs_allowed)
+        blob = blob + runs_allowed
+
+        runs_remaining = ConsumableRFIDUtilities._format_number_to_4_digit_str(runs_remaining)
+        blob = blob + runs_remaining
+        blob = ConsumableRFIDUtilities.add_checksum(tag_uid, blob)
+        return blob
+
+    @staticmethod
+    def get_anode_buffer_type_from_blob(blob: str) -> str:
+        buffer_type: str = ""
+        pieces: list[str] = blob.split(settings.SEPARATOR_CHAR)
+        if len(pieces) > 0:
+            buffer_type = pieces[0]
+        return buffer_type
+
+    @staticmethod
+    def get_anode_buffer_part_number_from_blob(blob: str) -> str:
+        part_num = ""
+        pieces: list[str] = blob.split(settings.SEPARATOR_CHAR)
+        if len(pieces) > 1:
+            part_num = pieces[1]
+        return part_num
+
+    @staticmethod
+    def get_anode_buffer_lot_number_from_blob(blob: str) -> str:
+        lot_num = ""
+        pieces = blob.split(settings.SEPARATOR_CHAR)
+        if len(pieces):
+            lot_num = pieces[2]
+        return lot_num
+
+    # public
+    # static
+    # long
+    # getAnodeBufferExpirationDateFromBlob(String
+    # blob) {
+    #     long
+    # expDate = 0L;
+    # String[]
+    # pieces = blob.split(",");
+    # if (pieces.length > 3)
+    # {
+    # try {
+    # expDate = new Long(pieces[3]);
+    # expDate *= 60000L;
+    # } catch (NumberFormatException var5) {
+    # }
+    # }
+    #
+    # return expDate;
+    # }
+
+    # public
+    # static
+    # long
+    # getAnodeBufferInstallationDateFromBlob(String
+    # blob) {
+    # long
+    # insDate = 0L;
+    # String[]
+    # pieces = blob.split(",");
+    # if (pieces.length > 4) {
+    # try {
+    # insDate = new Long(pieces[4]);
+    # insDate *= 60000L;
+    # } catch (NumberFormatException var5) {
+    # }
+    # }
+    #
+    # return insDate;
+    # }
+
+    # public
+    # static
+    # String
+    # setAnodeBufferInstallationDateInBlob(String
+    # tagUID, String
+    # blob, long
+    # insDate) {
+    # blob = removeChecksum(blob);
+    # String[]
+    # pieces = blob.split(",");
+    # pieces[4] = String.valueOf(insDate / 60000L);
+    # blob = "";
+    #
+    # for (int i = 0; i < pieces.length - 1; ++i) {
+    #     blob = blob + pieces[i] + ",";
+    # }
+    #
+    # blob = blob + pieces[pieces.length - 1];
+    # blob = addChecksum(tagUID, blob);
+    # return blob;
+
+    @staticmethod
+    def get_anode_buffer_life_on_instrument_from_blob(blob: str) -> int:
+        loi = 0
+        pieces: list[str] = blob.split(",")
+        if len(pieces) > 5:
+            try:
+                loi = int(pieces[5])
+                loi *= 60000
+            except ValueError as e:
+                print(e)
+        return loi
+
+    @staticmethod
+    def set_anode_buffer_life_on_instrument_in_blob(tag_uid: str, blob: str, loi: int) -> str:
+        blob = ConsumableRFIDUtilities._remove_checksum(blob)
+        pieces = blob.split(settings.SEPARATOR_CHAR)
+        pieces[5] = str(loi / 60000)
+        blob = settings.SEPARATOR_CHAR.join(blob)
+        blob = ConsumableRFIDUtilities.add_checksum(tag_uid, blob)
+        return blob
+
+    @staticmethod
+    def get_anode_buffer_allowed_runs_in_blob(blob: str) -> int:
+        allowed_runs = 0
+        pieces: list[str] = blob.split(settings.SEPARATOR_CHAR)
+        if len(pieces) > 6 and len(pieces) > 4:
+            try:
+                allowed_runs = int(pieces[6][:4])
+            except ValueError as e:
+                print(e)
+        return allowed_runs
+
+    @staticmethod
+    def set_anode_buffer_allowed_runs_ib_blob(tag_uid: str, blob: str, allowed_runs: int) -> str:
+        blob = ConsumableRFIDUtilities._remove_checksum(blob)
+        allowed_runs = max(allowed_runs, 0)
+        pieces: list[str] = blob.split(settings.SEPARATOR_CHAR)
+        current_str = ConsumableRFIDUtilities._format_number_to_4_digit_str(allowed_runs)
+        pieces[6] = pieces[6][4:] + current_str
+        blob = settings.SEPARATOR_CHAR.join(pieces)
+        blob = ConsumableRFIDUtilities.add_checksum(tag_uid, blob)
+        return blob
+
     @staticmethod
     def get_anode_buffer_remaining_runs_in_blob(blob: str) -> int:
         remaining_runs = 0
@@ -37,20 +204,6 @@ class ConsumableRFIDUtilities:
         return blob
 
     #  CHECKSUM FUNCTIONS
-    @staticmethod
-    def _calculate_v1_0_checksum(tag_uid: str, str_start_code: str, blob: str) -> str:
-        checksum = 0
-
-        try:
-            if tag_uid is not None:
-                checksum = binascii.crc32(tag_uid.encode("utf-8"))
-
-            checksum = binascii.crc32(str_start_code.encode("utf-8"), checksum)
-            checksum = binascii.crc32(blob.encode("utf-8"), checksum)
-        except UnicodeEncodeError as e:
-            print(e)
-
-        return str(checksum)
 
     @staticmethod
     def add_checksum(tag_uid: str, blob: str) -> str:
@@ -68,8 +221,8 @@ class ConsumableRFIDUtilities:
         return blob
 
     @staticmethod
-    def add_checksum(blob: str) -> str:
-        return ConsumableRFIDUtilities.add_checksum(None, blob)
+    # def add_checksum(blob: str) -> str:
+    #     return ConsumableRFIDUtilities.add_checksum(None, blob)
 
     @staticmethod
     def _remove_checksum(blob: str) -> str:
@@ -77,7 +230,7 @@ class ConsumableRFIDUtilities:
         return pieces[0]
 
     @staticmethod
-    def check_checksum(tag_uid: str, blob: str) -> bool:
+    def check_checksum(blob: str, tag_uid: str = None) -> bool:
         pieces: list[str] = blob.split(settings.CHECKSUM_SEPARATOR)
         test_checksum: str = (
             ConsumableRFIDUtilities.calculate_checksum(pieces[0])
@@ -97,35 +250,10 @@ class ConsumableRFIDUtilities:
             return checksum_valid
 
     @staticmethod
-    def check_v1_0_checksum(tag_uid: str, blob: str) -> bool:
-        pieces: list[str] = blob.split(settings.CHECKSUM_SEPARATOR)
-        test_checksum: str = (
-            ConsumableRFIDUtilities._calculate_v1_0_checksum(pieces[0])
-            if tag_uid is None
-            else ConsumableRFIDUtilities._calculate_v1_0_checksum(
-                tag_uid,
-                settings.ORIGINAL_SECRET_CHECKSUM_STRING,
-                pieces[0],
-            )
-        )
-
-        assert len(pieces) >= 2
-
-        if len(pieces) < 2:
-            return False
-        else:
-            checksum_valid = test_checksum == pieces[1]
-            return checksum_valid
-
-    @staticmethod
-    def check_checksum(blob: str) -> bool:
-        return ConsumableRFIDUtilities.check_checksum(None, blob)
-
-    @staticmethod
     def check_timestamp(blob: str) -> bool:
         pieces = blob.split("#")
         if len(pieces) < 3:
             return True
         else:
             timestamp = int(pieces[2], 36)
-            return time.time() >= timestamp
+            return int(datetime.datetime.now().strftime("milliseconds")) >= timestamp
--- tests/test_consumableRFIDUtilities.py
+++ tests/test_consumableRFIDUtilities.py
@@ -1,8 +1,45 @@
+from unittest import TestCase
+
 from src.consumableRFIDUtilities import ConsumableRFIDUtilities
 import unittest
 
+ANODE_BUFFER_VALID_BLOB = "ABC,3751713,0080623,28428480,0,20160,01000050#1rimda3#1qqh288#"
+ANODE_BUFFER_TAG_UID = ""
+
+
 class TestConsumableRFIDUtilities(unittest.TestCase):
     def test_get_anode_buffer_remaining_runs_in_blob(self):
-        self.assertEqual(ConsumableRFIDUtilities.get_anode_buffer_remaining_runs_in_blob("POP4,3751709,0060423,28330379,28085020,20160,00500960004408160005498#icnv4v#lrrhtdm7#1619999999"), 0)
+        self.assertEqual(ConsumableRFIDUtilities.get_anode_buffer_remaining_runs_in_blob(ANODE_BUFFER_VALID_BLOB), 50)
+        self.assertNotEqual(
+            ConsumableRFIDUtilities.get_anode_buffer_remaining_runs_in_blob(ANODE_BUFFER_VALID_BLOB), 100
+        )
+
+    def test_get_anode_buffer_allowed_runs_in_blob(self):
+        self.assertEqual(ConsumableRFIDUtilities.get_anode_buffer_allowed_runs_in_blob(ANODE_BUFFER_VALID_BLOB), 100)
+        self.assertNotEqual(ConsumableRFIDUtilities.get_anode_buffer_allowed_runs_in_blob(ANODE_BUFFER_VALID_BLOB), 50)
+
+    def test_check_timestamp(self):
+        self.assertEqual(ConsumableRFIDUtilities.check_timestamp(ANODE_BUFFER_VALID_BLOB), True)
+
+    def test_generate_blob(self):
+        self.assertEqual(
+            ConsumableRFIDUtilities.check_checksum(
+                ConsumableRFIDUtilities.create_anode_buffer_blob(
+                    tag_uid="",
+                    buffer_type="ABC",
+                    part_num="A053",
+                    lot_num="b343",
+                    expiration_date=1712437200,
+                    installation_date=0,
+                    life_on_instrument=0,
+                    runs_allowed=0,
+                    runs_remaining=0,
+                )
+            ),
+            True,
+        )
+
 
 # TODO: add real test cases for all functions which need to be written
+
+# TIP tag this line
